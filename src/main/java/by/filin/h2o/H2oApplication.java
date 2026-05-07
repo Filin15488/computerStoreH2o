3810e22c25930c49a57290709a63e56ac058340a
Pavel Filatov <filya135790@gmail.com>
2026-05-07 12:39:13 +0300

    init commit

---
+++ src/main/java/by/filin/h2o/H2oApplication.java
@@ -0,0 +1,13 @@
+package by.filin.h2o;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2oApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2oApplication.class, args);
+	}
+
+}
